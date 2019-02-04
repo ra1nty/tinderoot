c35f95b729e9e947617b6d26ff45193d625baa3d
yutang2 <yutang2@illinois.edu>
2019-02-04 01:40:28 -0600

    added tests for login/register

--- tinderoot/config.py
+++ tinderoot/config.py
@@ -28,7 +28,24 @@ class Production(Config):
     SQLALCHEMY_ECHO = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
+class Testing(Config):
+    """
+    Testing config
+    """
+    SECRET_KEY = ""
+    _SQLALCHEMY_DATABASE_DATABASE = 'travis_ci_test'
+    _SQLALCHEMY_DATABASE_HOSTNAME = 'localhost'
+    _SQLALCHEMY_DATABASE_PASSWORD = ''
+    _SQLALCHEMY_DATABASE_USERNAME = 'postgres'
+    SQLALCHEMY_DATABASE_URI = 'postgres://{u}:{p}@{h}/{d}'.format(
+        d=_SQLALCHEMY_DATABASE_DATABASE, h=_SQLALCHEMY_DATABASE_HOSTNAME,
+        p=_SQLALCHEMY_DATABASE_PASSWORD, u=_SQLALCHEMY_DATABASE_USERNAME
+    )
+    SQLALCHEMY_ECHO = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 app_config = {
     'development' : Development,
-    'production': Production
+    'production' : Production,
+    'testing' : Testing
 }
